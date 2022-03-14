103a2ce3a3c5892950b1615406dccae0fc1a39b3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-14 19:15:22 -0400

    print report in the example after each spec

--- examples/all-files/cypress/plugins/index.js
+++ examples/all-files/cypress/plugins/index.js
@@ -2,5 +2,6 @@ module.exports = (on, config) => {
   require('../../../../task')(on, config)
   // instrument the specs and any source files loaded from specs
   on('file:preprocessor', require('../../../../use-babelrc'))
+
   return config
 }
--- examples/multiple-files/cypress/plugins/index.js
+++ examples/multiple-files/cypress/plugins/index.js
@@ -1,5 +1,17 @@
+const NYC = require('nyc')
+const nyc = new NYC({
+  cwd: process.cwd(),
+  reporter: 'text',
+})
+
 module.exports = (on, config) => {
   require('../../../../task')(on, config)
   on('file:preprocessor', require('../../../../use-babelrc'))
+
+  on('after:spec', (t) => {
+    console.log('after spec %s', t.relative)
+    return nyc.report()
+  })
+
   return config
 }
