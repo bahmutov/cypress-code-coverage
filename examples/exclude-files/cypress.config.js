17deb5afb6a73433a271c10027d734f2a39a7e02
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-24 14:09:27 -0400

    fix exclude files

---
+++ examples/exclude-files/cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:1234',
+    specPattern: 'cypress/integration/*.js',
+    coverage: {
+      exclude: true,
+    },
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})
