17067468ddbfd65a96f72efc8365680e9dfb8162
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-24 14:07:36 -0400

    fix the test isolation

--- examples/before-all-visit/cypress/integration/spec.js
+++ examples/before-all-visit/cypress/integration/spec.js
@@ -1,19 +1,19 @@
 /// <reference types="cypress" />
-describe('coverage information', () => {
-  before(() => {
-    cy.log('visiting index.html')
-    cy.visit('index.html')
-  })
+describe(
+  'coverage information',
+  { testIsolation: false, viewportHeight: 100, viewportWidth: 100 },
+  () => {
+    before(() => {
+      cy.log('visiting index.html')
+      cy.visit('index.html')
+    })
 
-  it('calls add', () => {
-    cy.window()
-      .invoke('add', 2, 3)
-      .should('equal', 5)
-  })
+    it('calls add', () => {
+      cy.window().invoke('add', 2, 3).should('equal', 5)
+    })
 
-  it('calls sub', () => {
-    cy.window()
-      .invoke('sub', 2, 3)
-      .should('equal', -1)
-  })
-})
+    it('calls sub', () => {
+      cy.window().invoke('sub', 2, 3).should('equal', -1)
+    })
+  },
+)
