3eb043048fb2a2413d91a0198274d6a77c275be5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-25 09:58:20 -0400

    update the fullstack example

---
+++ examples/fullstack/cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    fixturesFolder: false,
+    baseUrl: 'http://localhost:3003',
+    env: {
+      codeCoverage: {
+        url: 'http://localhost:3003/__coverage__',
+      },
+      coverage: {
+        exclude: true,
+      },
+    },
+    video: false,
+    specPattern: 'cypress/integration/spec.js',
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})
