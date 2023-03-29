aa709cd8360df8405e4adc36c032a49e9a987a11
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-29 08:38:02 -0400

    check two files

--- examples/component/src/Hello.cy.jsx
+++ examples/component/src/Hello.cy.jsx
@@ -3,6 +3,6 @@ import Hello from './Hello'
 describe('Hello', () => {
   it('shows the greeting', () => {
     cy.mount(<Hello greeting="World" />)
-    cy.contains('Hello, World')
+    cy.contains('Hello, World').should('include.text', 'sum is 5')
   })
 })
