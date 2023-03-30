6b11c9ed72b792d73b56c2bfdbcc0c9f3068fcbc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-29 22:43:25 -0400

    feat: add quiet mode

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
   viewportHeight: 200,
   viewportWidth: 200,
   e2e: {
+    env: {
+      coverage: {
+        // set to true to hide the messages in the Command Log
+        quiet: false,
+      },
+    },
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
