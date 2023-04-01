433cba3bb9a7bcb65bfa0f3606c603536306852b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-04-01 16:17:39 -0400

    copy found coverage files

--- bin/cc-merge.js
+++ bin/cc-merge.js
@@ -11,4 +11,41 @@ if (!topCoverageFolder) {
   console.error('use: npx cc-merge <top coverage folder>')
   process.exit(1)
 }
-debug('looking for coverage-final.json files in %s', topCoverageFolder)
+
+const coverageFilename = 'coverage-final.json'
+debug('looking for %s files in %s', coverageFilename, topCoverageFolder)
+
+const fs = require('fs')
+const path = require('path')
+const globby = require('globby')
+const allFiles = globby.sync(`**/${coverageFilename}`, {
+  absolute: true,
+  cwd: topCoverageFolder,
+})
+debug('found %d coverage files', allFiles.length)
+debug(allFiles.join(','))
+
+if (!allFiles.length) {
+  console.error(
+    'Could not find any %s files in the folder %s',
+    coverageFilename,
+    topCoverageFolder,
+  )
+  process.exit(1)
+}
+
+const nycOutputFolder = '.nyc_output'
+if (fs.existsSync(nycOutputFolder)) {
+  debug('deleting the existing folder %s', nycOutputFolder)
+  fs.rmSync(nycOutputFolder, { recursive: true })
+}
+
+if (!fs.existsSync(nycOutputFolder)) {
+  debug('creating folder %s', nycOutputFolder)
+  fs.mkdirSync(nycOutputFolder)
+}
+allFiles.forEach((filename, k) => {
+  const outputFilename = path.join(nycOutputFolder, `c-${k + 1}.json`)
+  fs.copyFileSync(filename, outputFilename)
+  debug('%d: copied %s to %s', k + 1, filename, outputFilename)
+})
