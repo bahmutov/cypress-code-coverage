5445fbdd56bf0a98bd8af1fa93dc67687cb95e19
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-04-03 09:51:01 -0400

    feat: working on gha summary output

--- plugin.js
+++ plugin.js
@@ -1,9 +1,11 @@
 // @ts-check
 require('console.table')
 const { getNycReportFilename } = require('./task-utils')
-const { existsSync } = require('fs')
+const { existsSync, readFileSync } = require('fs')
 const NYC = require('nyc')
 const debug = require('debug')('code-coverage')
+const ghCore = require('@actions/core')
+const path = require('path')
 
 const nycFilename = getNycReportFilename(process.cwd())
 
@@ -45,6 +47,62 @@ function registerCodeCoveragePlugin(on, config) {
         console.warn('Could not find coverage file %s', nycFilename)
       }
     })
+
+    if (process.env.GITHUB_ACTIONS) {
+      debug('will report code coverage on GitHub Actions')
+      const summaryFilename = path.join('coverage', 'coverage-summary.json')
+      if (!existsSync(summaryFilename)) {
+        debug('cannot find summary file %s', summaryFilename)
+      } else {
+        const summary = JSON.parse(readFileSync(summaryFilename, 'utf8'))
+        if (summary.total) {
+          debug('code coverage summary totals %o', summary.total)
+          const s = summary.total.statements
+          const b = summary.total.branches
+          const f = summary.total.functions
+          const l = summary.total.lines
+          const row = [
+            s.pct,
+            `${s.covered}/${s.total}`,
+            b.pct,
+            `${b.covered}/${b.total}`,
+            f.pct,
+            `${f.covered}/${f.total}`,
+            l.pct,
+            `${l.covered}/${l.total}`,
+          ]
+
+          // only output the GitHub summary table AFTER the run
+          // because GH does not show the summary before the job finishes
+          // so we might as well wait for all spec results to come in
+          on('after:run', () => {
+            // https://github.blog/2022-05-09-supercharging-github-actions-with-job-summaries/
+            ghCore.summary
+              .addHeading('Code coverage')
+              .addTable([
+                [
+                  { data: 'Statements %', header: true },
+                  { data: '', header: true },
+                  { data: 'Branches %', header: true },
+                  { data: '', header: true },
+                  { data: 'Functions %', header: true },
+                  { data: '', header: true },
+                  { data: 'Lines %', header: true },
+                  { data: '', header: true },
+                ],
+                row,
+              ])
+              .addLink(
+                '@bahmutov/cypress-code-coverage',
+                'https://github.com/bahmutov/cypress-code-coverage',
+              )
+              .write()
+          })
+        } else {
+          debug('could not find totals in %s', summaryFilename)
+        }
+      }
+    }
   }
 
   // IMPORTANT to return the config object
