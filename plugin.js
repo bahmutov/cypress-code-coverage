504f40c857d0638d56bf822db9a136e9234f43ff
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-04-03 10:03:34 -0400

    arrange calls

--- plugin.js
+++ plugin.js
@@ -34,6 +34,7 @@ function registerCodeCoveragePlugin(on, config) {
     }
   }
 
+  debug('should report? %s', shouldReport)
   if (shouldReport) {
     const nyc = new NYC({
       cwd: process.cwd(),
@@ -50,58 +51,61 @@ function registerCodeCoveragePlugin(on, config) {
 
     if (process.env.GITHUB_ACTIONS) {
       debug('will report code coverage on GitHub Actions')
-      const summaryFilename = path.join('coverage', 'coverage-summary.json')
-      if (!existsSync(summaryFilename)) {
-        debug('cannot find summary file %s', summaryFilename)
-      } else {
-        const summary = JSON.parse(readFileSync(summaryFilename, 'utf8'))
-        if (summary.total) {
-          debug('code coverage summary totals %o', summary.total)
-          const s = summary.total.statements
-          const b = summary.total.branches
-          const f = summary.total.functions
-          const l = summary.total.lines
-          const row = [
-            s.pct,
-            `${s.covered}/${s.total}`,
-            b.pct,
-            `${b.covered}/${b.total}`,
-            f.pct,
-            `${f.covered}/${f.total}`,
-            l.pct,
-            `${l.covered}/${l.total}`,
-          ]
-
-          // only output the GitHub summary table AFTER the run
-          // because GH does not show the summary before the job finishes
-          // so we might as well wait for all spec results to come in
-          on('after:run', () => {
-            // https://github.blog/2022-05-09-supercharging-github-actions-with-job-summaries/
-            ghCore.summary
-              .addHeading('Code coverage')
-              .addTable([
-                [
-                  { data: 'Statements %', header: true },
-                  { data: '', header: true },
-                  { data: 'Branches %', header: true },
-                  { data: '', header: true },
-                  { data: 'Functions %', header: true },
-                  { data: '', header: true },
-                  { data: 'Lines %', header: true },
-                  { data: '', header: true },
-                ],
-                row,
-              ])
-              .addLink(
-                '@bahmutov/cypress-code-coverage',
-                'https://github.com/bahmutov/cypress-code-coverage',
-              )
-              .write()
-          })
+      on('after:run', () => {
+        const summaryFilename = path.join('coverage', 'coverage-summary.json')
+        if (!existsSync(summaryFilename)) {
+          debug('cannot find summary file %s', summaryFilename)
         } else {
-          debug('could not find totals in %s', summaryFilename)
+          const summary = JSON.parse(readFileSync(summaryFilename, 'utf8'))
+          if (summary.total) {
+            debug('code coverage summary totals %o', summary.total)
+            const s = summary.total.statements
+            const b = summary.total.branches
+            const f = summary.total.functions
+            const l = summary.total.lines
+            const row = [
+              s.pct,
+              `${s.covered}/${s.total}`,
+              b.pct,
+              `${b.covered}/${b.total}`,
+              f.pct,
+              `${f.covered}/${f.total}`,
+              l.pct,
+              `${l.covered}/${l.total}`,
+            ]
+            debug(row)
+
+            // only output the GitHub summary table AFTER the run
+            // because GH does not show the summary before the job finishes
+            // so we might as well wait for all spec results to come in
+            on('after:run', () => {
+              // https://github.blog/2022-05-09-supercharging-github-actions-with-job-summaries/
+              ghCore.summary
+                .addHeading('Code coverage')
+                .addTable([
+                  [
+                    { data: 'Statements %', header: true },
+                    { data: '', header: true },
+                    { data: 'Branches %', header: true },
+                    { data: '', header: true },
+                    { data: 'Functions %', header: true },
+                    { data: '', header: true },
+                    { data: 'Lines %', header: true },
+                    { data: '', header: true },
+                  ],
+                  row,
+                ])
+                .addLink(
+                  '@bahmutov/cypress-code-coverage',
+                  'https://github.com/bahmutov/cypress-code-coverage',
+                )
+                .write()
+            })
+          } else {
+            debug('could not find totals in %s', summaryFilename)
+          }
         }
-      }
+      })
     }
   }
 
